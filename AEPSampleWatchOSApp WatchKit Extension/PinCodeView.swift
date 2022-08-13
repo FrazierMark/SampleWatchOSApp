654ed7d59ac2001b99cb3e37af6bbc11eb86f7d0
Mark Frazier <60164353+FrazierMark@users.noreply.github.com>
2022-08-13 08:30:03 -0700

    Beginning to look at assurance

--- AEPSampleWatchOSApp WatchKit Extension/PinCodeView.swift
+++ AEPSampleWatchOSApp WatchKit Extension/PinCodeView.swift
@@ -39,8 +39,13 @@ struct Home: View {
 
 
 struct LockScreen: View {
+    
+    @State var password = ""
+
+    
     var body: some View {
         
+                
         VStack{
             HStack{
                 Spacer(minLength: 0)
@@ -73,9 +78,53 @@ struct LockScreen: View {
                 .frame(width: 20, height: 20)
                 .padding(.top, 5)
             
+            Text("Enter Pin to Connect to Assurance")
+                .font(.title3)
+                .fontWeight(.medium)
+                .padding(.top, 4)
+            
+            HStack(spacing: 22){
+                // Passord CVircle view
+                
+                ForEach(0..<4, id: \.self){index in
+                    PasswordView(index: index, password: $password)
+                }
+                
+            }
+            .padding(.top, 5)
+            
             Spacer(minLength: 0)
         }
         .navigationTitle("")
         .navigationBarHidden(true)
+    }
+}
+
+
+struct PasswordView: View {
+    var index: Int
+    @Binding var password: String
+    
+    var body: some View {
+        
+        ZStack {
+            
+            Circle()
+                .stroke(Color.white, lineWidth: 1)
+                .frame(width: 4, height: 4)
+            
+            //checking whether it is typed
+            
+            if password.count > index{
+                
+                Circle()
+                    .fill(Color.white)                    .frame(width: 4, height: 4)
+
+                
+            }
+            
+        }
+        
+        
     }
 }
